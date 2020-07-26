e5952e034ed7d2623b8630889db3fd398146d36a
Enn Michael <enntheprogrammer@gmail.com>
2020-07-26 20:55:37 +0200

    Added adjacent_positions and tests

--- src/checkers.rs
+++ src/checkers.rs
@@ -1,6 +1,8 @@
+use arrayvec::ArrayVec;
+
 pub struct CheckersGame {
     table: Table,
-    active_team: Team,
+    team_on_turn: Team,
 }
 
 pub type Table = [Option<Piece>; 32];
@@ -19,11 +21,31 @@ pub enum Team {
 impl CheckersGame {
     /// Returns those pieces which must be moved on next turn.
     /// If there are no mandatory pieces, returns an empty vector.
-    pub fn mandatory_pieces(&self) -> Vec<usize> {}
+    pub fn mandatory_pieces(&self) -> Vec<usize> {
+        vec![]
+    }
+
+    fn adjacent_positions(pos: usize) -> impl Iterator<Item = usize> {
+        let row = (pos / 4) as i32;
+        let offset: i32 = if row % 2 == 0 { 4 } else { 5 };
+
+        ArrayVec::from([
+            pos as i32 - offset,
+            pos as i32 - offset + 1,
+            pos as i32 - offset + 8,
+            pos as i32 - offset + 9,
+        ])
+        .into_iter()
+        .filter(move |&p| {
+            let p_row = p / 4;
+            0 <= p && p < 32 && (p_row - row).abs() == 1
+        })
+        .map(|p| p as usize)
+    }
 
     /// Returns the team which is currently on turn.
-    pub fn active_team(&self) -> Team {
-        self.active_team
+    pub fn team_on_turn(&self) -> Team {
+        self.team_on_turn
     }
 
     /// Jump piece at position from to position to.
@@ -36,5 +58,38 @@ impl CheckersGame {
 
     pub fn table(&self) -> &Table {
         &self.table
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn adjacent_positions() {
+        assert_eq!(
+            CheckersGame::adjacent_positions(0).collect::<Vec<usize>>(),
+            vec![4, 5]
+        );
+        assert_eq!(
+            CheckersGame::adjacent_positions(12).collect::<Vec<usize>>(),
+            vec![8, 16]
+        );
+        assert_eq!(
+            CheckersGame::adjacent_positions(9).collect::<Vec<usize>>(),
+            vec![5, 6, 13, 14]
+        );
+        assert_eq!(
+            CheckersGame::adjacent_positions(11).collect::<Vec<usize>>(),
+            vec![7, 15]
+        );
+        assert_eq!(
+            CheckersGame::adjacent_positions(27).collect::<Vec<usize>>(),
+            vec![23, 31]
+        );
+        assert_eq!(
+            CheckersGame::adjacent_positions(28).collect::<Vec<usize>>(),
+            vec![24]
+        );
     }
 }
